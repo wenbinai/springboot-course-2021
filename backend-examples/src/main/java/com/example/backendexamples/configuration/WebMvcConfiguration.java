da86ecca84a86e850a95f029d5d4788729ad05cb
BO <bwang0629@gmail.com>
2021-05-17 23:08:03 +0800

    backend-examples CorsFilter启动跨域

--- backend-examples/src/main/java/com/example/backendexamples/configuration/WebMvcConfiguration.java
+++ backend-examples/src/main/java/com/example/backendexamples/configuration/WebMvcConfiguration.java
@@ -5,7 +5,12 @@ import com.example.backendexamples.interceptor.LoginInterceptor;
 import com.example.backendexamples.interceptor.TeacherInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -29,4 +34,19 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
                 .addPathPatterns("/api/admin/**");
     }
 
+    // 后端允许跨域请求
+    // 生产环境下，后端应隐藏请求接口，由前端通过部署时反向代理实现跨域请求。例如通过nginx服务器
+    @Bean
+    @Order(0)
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource configSource
+                = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(configSource);
+    }
 }
